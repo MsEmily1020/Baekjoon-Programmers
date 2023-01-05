6b0c0b20058de4dcb439be4ac950115b3dba592e
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-05 21:36:39 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 6; ; i+=6) {
+            answer++;
+            if(i % n == 0) return answer;  
+        }
+    }
+}
