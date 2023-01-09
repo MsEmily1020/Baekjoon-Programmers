6deba3db9c49f3f6276f1f5012cb2c2469571abb
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-09 17:38:44 +0900

    [Bronze III] Title: Time to Decompress, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int N = sc.nextInt();
+			char c = sc.next().charAt(0);
+			for(int j = 0; j < N; j++) System.out.print(c);
+			System.out.println();
+		}
+	}
+}
