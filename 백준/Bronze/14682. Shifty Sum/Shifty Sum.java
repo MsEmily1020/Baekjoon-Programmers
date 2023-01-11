1908c4b301d9a1a8845642d17ce48acc05603fff
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-11 15:19:37 +0900

    [Bronze III] Title: Shifty Sum, Time: 236 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze/14682. Shifty Sum/Shifty Sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int sum = N;
+		for (int i = 0; i < k; i++) {
+			N = Integer.parseInt(N + "0");
+			sum += N;
+		}
+		
+		System.out.println(sum);
+	}
+}
