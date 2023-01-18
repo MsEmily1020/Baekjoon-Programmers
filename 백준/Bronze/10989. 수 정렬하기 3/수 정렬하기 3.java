dc364e1b90bab00cf6d4f86d4af222a2bfbe5f8c
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-18 21:49:52 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2664 ms, Memory: 345728 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] arr = new int[Integer.parseInt(br.readLine())];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			bw.write(String.valueOf(arr[i]));
+			bw.newLine();
+		}
+		bw.close();
+		br.close();
+	}
+}
