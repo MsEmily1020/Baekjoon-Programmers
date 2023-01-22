06f5fe9310e88b93b19ef58632363ff4e4e8c8ee
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-22 20:36:27 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 224 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i < N; i++) {
+			System.out.print(i + " ");
+			if(i % 6 == 0) System.out.print("Go! ");
+		}
+		System.out.print(N + " Go!");
+	}
+}
