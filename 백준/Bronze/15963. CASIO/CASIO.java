6ff9d4b205f1b976f6a6c388053b2c8b370055f3
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-24 13:20:58 +0900

    [Bronze IV] Title: CASIO, Time: 216 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		if(sc.nextLong() == sc.nextLong()) System.out.println(1);
+		else System.out.println(0);
+	}
+}
