1a72e33b245566b80d77501e847d21d5de0dc7ca
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-30 11:55:57 +0900

    [Bronze IV] Title: アイスクリーム (Ice Cream), Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/24087. アイスクリーム （Ice Cream）/アイスクリーム （Ice Cream）.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int S = sc.nextInt(), A = sc.nextInt(), B = sc.nextInt();
+		int cnt = 0;
+		while(S > A) {
+			A += B;
+			cnt+=100;
+		}
+		System.out.println(250 + cnt);
+	}
+}
