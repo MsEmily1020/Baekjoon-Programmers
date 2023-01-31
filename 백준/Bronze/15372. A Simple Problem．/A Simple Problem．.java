8be9aa9773d0682b615beb286480ab7b03a3a949
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-01-31 16:51:05 +0900

    [Bronze IV] Title: A Simple Problem., Time: 1452 ms, Memory: 50116 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++)
+			System.out.println((long)Math.pow(Long.parseLong(br.readLine()), 2));
+	}
+}
