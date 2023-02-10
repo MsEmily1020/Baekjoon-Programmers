0b4e4846e7c7f3b93d99151f9c4cfa0e3f1f513a
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-02-10 16:02:01 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 1732 ms, Memory: 32160 KB -BaekjoonHub

--- 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.java
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.java
@@ -1,61 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
+	public static void print1(int N) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N * 3; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void print2(int N) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N * 5; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print("@");
-			}
-			for (int j = 0; j < N * 3; j++) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < N; j++) {
-				System.out.print("@");
-			}
-			System.out.println();
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N * 5; j++) {
-				System.out.print("@");
-			}
-			System.out.println();
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print("@");
-			}
-			for (int j = 0; j < N * 3; j++) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < N; j++) {
-				System.out.print("@");
-			}
-			System.out.println();
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N * 5; j++) {
-				System.out.print("@");
-			}
-			System.out.println();
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print("@");
-			}
-			for (int j = 0; j < N * 3; j++) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < N; j++) {
-				System.out.print("@");
-			}
-			System.out.println();
-		}
+		print1(N); print2(N); print1(N); print2(N); print1(N);
 	}
 }
