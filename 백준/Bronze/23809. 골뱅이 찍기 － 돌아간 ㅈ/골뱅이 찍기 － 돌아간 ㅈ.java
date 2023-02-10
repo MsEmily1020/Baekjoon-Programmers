d6ec084d81dbbd0c1eab1d1118fe9304d9e3f7bb
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-02-10 15:53:36 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 1544 ms, Memory: 33512 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N * 3; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N * 2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N * 3; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N * 2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N * 3; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+	}
+}
