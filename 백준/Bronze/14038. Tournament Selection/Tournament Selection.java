5aef8d6a6d975a0cd6974b02cc0af556983ee61e
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-02-23 10:14:16 +0900

    [Bronze IV] Title: Tournament Selection, Time: 200 ms, Memory: 17384 KB -BaekjoonHub

---
+++ 백준/Bronze/14038. Tournament Selection/Tournament Selection.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = 0;
+		for (int i = 0; i < 6; i++)
+			if(sc.next().equals("W")) cnt++;
+		if(cnt >= 5) System.out.println(1);
+		else if(cnt >= 3) System.out.println(2);
+		else if(cnt >= 1) System.out.println(3);
+		else System.out.println(-1);
+	}
+}
