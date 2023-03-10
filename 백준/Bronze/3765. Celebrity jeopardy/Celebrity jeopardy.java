c81d51b5024cd7d775bce390bb33b422a16312c9
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-03-11 00:28:10 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 120 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			String s = br.readLine();
+			if(s == null) break;
+			System.out.println(s);
+		}
+	}
+}
