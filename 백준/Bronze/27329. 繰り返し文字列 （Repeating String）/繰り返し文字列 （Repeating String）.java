043e052583346aa45eb7c9870bb88662ec3b5ca9
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-05-12 15:12:39 +0900

    [Bronze III] Title: 繰り返し文字列 (Repeating String), Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/27329. 繰り返し文字列 （Repeating String）/繰り返し文字列 （Repeating String）.java
+++ 백준/Bronze/27329. 繰り返し文字列 （Repeating String）/繰り返し文字列 （Repeating String）.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sc.nextInt();
-		String str = sc.next();
-		String answer1 = str.substring(0, str.length() / 2);
-		String answer2 = str.substring(str.length() / 2);
-		
-		System.out.println(answer1.equals(answer2) ? "Yes" : "No");
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        br.readLine();
+        String str = br.readLine();
+        String answer1 = str.substring(0, str.length() / 2);
+        String answer2 = str.substring(str.length() / 2);
+
+        System.out.println(answer1.equals(answer2) ? "Yes" : "No");
+    }
 }
