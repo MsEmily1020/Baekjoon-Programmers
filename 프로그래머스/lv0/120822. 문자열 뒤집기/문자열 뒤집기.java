ec37f614e08d9c6676e1006816721d030f9e5ebe
Emily <121646949+MsEmily1020@users.noreply.github.com>
2023-06-05 00:21:31 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.08 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        for(int i = my_string.length() - 1; i >= 0; i--) {
-            answer += my_string.charAt(i);
-        }
+        answer = new StringBuffer(my_string).reverse().toString();
         return answer;
     }
 }
