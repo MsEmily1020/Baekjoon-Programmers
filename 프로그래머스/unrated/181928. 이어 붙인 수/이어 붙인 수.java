1a2ea4be469eefbd43b40bdbf86c0a58014111d4
김혜승 <121646949+MsEmily1020@users.noreply.github.com>
2023-11-26 12:07:49 +0900

    [level unrated] Title: 이어 붙인 수, Time: 10.46 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "", even = "";
+        for (int num : num_list)
+            if(num % 2 == 0) even += num;
+            else odd += num;
+    
+        return Integer.parseInt(odd) + Integer.parseInt(even);
+    }
+}
