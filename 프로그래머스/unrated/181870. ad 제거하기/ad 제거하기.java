56c70c1109178d9f7988144a1c8efd60bc7bd75a
김혜승 <121646949+MsEmily1020@users.noreply.github.com>
2024-01-08 21:37:41 +0900

    [level unrated] Title: ad 제거하기, Time: 0.65 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public List solution(String[] strArr) {
+        List<String> answer = new ArrayList<>();
+        for(String str : strArr) if(!str.contains("ad")) answer.add(str);
+        return answer;
+    }
+}
